962d6bbb5a7615f46813a952229d39de53186c4b
mikael wawrziczny <mikaelwaw@gmail.com>
2021-04-02 09:11:57 +0200

    final version

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ app.use(cookieSession({
 }))
 
 // update body parser reading, read and convert the serveur request
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 // clean enter data before send to database
 const sanitizerPlugin = require('mongoose-sanitizer-plugin')
-const sauceValidator = require('../middleware/sauceValidator')
 
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: { type: String, required: true, validate: sauceValidator.nameValidator },
-  manufacturer: { type: String, required: true, validate: sauceValidator.manufacturerValidator },
-  description: { type: String, required: true, validate: sauceValidator.descriptionValidator },
-  mainPepper: { type: String, required: true, validate: sauceValidator.mainPepperValidator },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number, required: true },
